782ebd1604290b2584a4e45da5f4ba2344837697
aurorakb <auroraklberg@gmail.com>
2016-02-08 11:53:49 -0800

    Convert into 5 second strength challenge

--- read_serial.py
+++ read_serial.py
@@ -4,6 +4,8 @@ from __future__ import print_function
 
 import serial
 import os
+import time
+import sys
 from birdy.twitter import UserClient
 
 CONSUMER_KEY = 'gU90Ph9KvpUhWerI8u7Wei5rz'
@@ -20,24 +22,37 @@ twitter_client = UserClient(CONSUMER_KEY,
 THRESHOLD = 900
 
 def main():
-    recent_values = []
+    name = get_name()
+    print("Hello %s " % name)
+    all_values = []
     with serial.Serial('/dev/tty.usbmodem1411', 9600) as port:
         line_read = []
-        while True:
-            last_data_read = port.read(10)
+        start_time = None
+        has_started = False
+        while not has_started or time.time() - start_time < 5:
+            last_data_read = port.read(20)
+            if not has_started:
+                print("You have 5 seconds to flex. GO!")
+                has_started = True
+                start_time = time.time()
             for character in last_data_read:
                 if character == '\n':
                     value_line = ''.join(line_read)
                     value = int(value_line.split('=')[1].strip())
-                    # recent_values.append(value)
-                    if value > THRESHOLD:
-                        twitter_client.api.statuses.update.post(status='I just flexed %d JIGGAWATTS!' % value)
-                        print('Tweeted!')
-
-                    print(value_line)
+                    all_values.append(value)
+                    sys.stdout.write("\r%d  " % value)
+                    sys.stdout.flush()
                     line_read = []
                 else:
                     line_read.append(character)
+        max_value = max(all_values)
+
+        twitter_client.api.statuses.update.post(status='%s just flexed %d JIGGAWATTS!' % (name, max_value))
+        print('\nGreat - you just flexed %d JIGGAWATTS' % max_value)
+
+
+def get_name():
+    return raw_input("Enter your name: ")
 
 
 
